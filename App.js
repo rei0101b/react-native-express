a1b12a39d042827df1ff3139053eda42301a5667
rei0101b <rei0101b@gmail.com>
2018-03-27 17:02:00 +0900

    Text

--- App.js
+++ App.js
@@ -1,68 +1,28 @@
 import React, { Component } from 'react'
-import { AppRegistry, View, StyleSheet } from 'react-native'
-import Toggle from './Toggle'
+import { AppRegistry, View, Text, StyleSheet } from 'react-native'
 
 class App extends Component {
-  state = {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-
   render() {
-    const {flexDirection, alignItems, justifyContent} = this.state
-    const layoutStyle = {flexDirection, justifyContent, alignItems}
-
-    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
-    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
-
     return (
       <View style={styles.container}>
-        <Toggle
-          label={'Primary axix (flexDirection)'}
-          value={flexDirection}
-          options={['row', 'column']}
-          onChange={(option) => this.setState({flexDirection: option})}
-        />
-        <Toggle
-          label={primaryAxis + ' distrubution (justifyContent)'}
-          value={justifyContent}
-          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
-          onChange={(option) => this.setState({justifyContent: option})}
-        />
-        <Toggle
-          label={secondaryAxis + ' alignment (alignItems)'}
-          value={alignItems}
-          options={['flex-start', 'center', 'flex-end', 'stretch']}
-          onChange={(option) => this.setState({alignItems: option})}
-        />
-        <View style={[styles.layout, layoutStyle]}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
-        </View>
+        <Text style={styles.text}>Hello!</Text>
       </View>
     )
-   }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    margin: 50,
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  layout: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.05)',
-  },
-  box: {
-    padding: 25,
-    backgroundColor: 'steelblue',
-    margin: 5,
+  text: {
+    backgroundColor: 'whitesmoke',
+    color: '#4A90E2',
+    fontSize: 24,
+    padding: 10,
   },
 })
-
-
-
 
 export default AppRegistry.registerComponent(App)
